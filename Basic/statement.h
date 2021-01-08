57505ae38a70e7fd3266f5acf3db2f6eca9a1914
Amekiri <72453752+Zhang-CH3@users.noreply.github.com>
2021-01-08 08:37:30 +0800

    Update statement.h

--- Basic/statement.h
+++ Basic/statement.h
@@ -87,7 +87,7 @@ public:
     LineState(int t,int num=0):
         type(t),lineNum(num){}
 };
-Statement *parseState(string line);
+Statement *interpret(string line);
 
 class Rem : public Statement {
 public:
